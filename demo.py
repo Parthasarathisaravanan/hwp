d2df1469b590e734638ec4ccdc015a6d91d31a3c
Parthasarathi saravanan <33322740+Parthasarathisaravanan@users.noreply.github.com>
2020-10-25 16:21:22 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,23 @@
+from flask import Flask,redirect,url_for, request,render_template
+import joblib
+import numpy as np
+ 
+
+app = Flask(__name__)
+res=joblib.load('model.pkl')
+
+@app.route('/')
+def index():
+    
+    return render_template('page.html')
+
+@app.route('/result', methods=['post', 'get'])
+def login():
+    if request.method == 'POST':
+         weight = request.form["weight"]
+         weight=int(weight)
+         weight=res.predict([[weight]])
+    return  render_template('page.html',val=weight)
+   
+if __name__ == '__main__':
+   app.run(debug = True) 
